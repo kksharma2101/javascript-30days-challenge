d648243f3c97dceefc952ad2208afd06b997f409
kamal sharma <kamalbsr2101@gmail.com>
2024-07-13 08:23:04 +0530

    first commit add to day-1 assignment

---
+++ day-1.js
@@ -0,0 +1,42 @@
+// Activity 1: Variable Declaration 
+var num = 21;
+// console.log(num)
+
+let str = "kamal";
+// console.log(str)
+
+// Activity 2: Constant Declaration 
+
+const booleanVal = true;
+// console.log(booleanVal)
+
+// Activity 3: Data Types
+
+let numDataType = 50;
+let strDataType = "kamal sharma";
+let objectDataType = {}; // todo
+let arrayDataType = [];  // todo
+let booleanDataType = true;
+
+// console.log(typeof(numDataType))
+// console.log(typeof(strDataType))
+// console.log(typeof(objectDataType))
+// console.log(typeof(arrayDataType))
+// console.log(typeof(booleanDataType))
+
+// Activity 4: Reassigning Variables
+
+let assignVal = 99;
+// console.log(assignVal)
+assignVal = 550;
+// console.log(assignVal)
+
+
+// Activity 5: Understanding const
+
+const reassignConst = "Developer";
+// console.log(reassignConst);
+reassignConst = "technology";
+// console.log(reassignConst) // Give the type error, reassignment the constant variable
+
+// Activity 6: Understanding const
